50bb3dde8c7d65218d4d48d621302e8883d93f29
钩吻 <pinian.lpn@alibaba-inc.com>
2016-06-10 00:10:05 +0800

    Distributed session used redis or memcached

--- mvn-repository/github-deply.bat
+++ mvn-repository/github-deply.bat
@@ -1,4 +1,12 @@
 del com/
+
 cd ../dsessn/
 call mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
+cd ../dsessn-jetty/
+call mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
+cd ../dsessn-tomcat/
+call mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
 @pause
--- mvn-repository/github-deply.sh
+++ mvn-repository/github-deply.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 rm -rf com/
+
 cd ../dsessn/
+mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
+cd ../dsessn-jetty/
+mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+
+cd ../dsessn-tomcat/
 mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
